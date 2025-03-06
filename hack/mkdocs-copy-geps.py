948b2339e6c4850d2e7f005b27ef19261a17a3cb
Blake Covarrubias <bcovarrubias@google.com>
2025-03-06 05:58:08 +0000

    docs: Fix 'mkdocs serve' endless build loop
    When developing docs and previewing docs using `mkdocs serve`, the site
    gets stuck into an endless build loop when any content is changed. This
    is caused by two hooks that copy content into the site-src/ directory on
    site build, mkdocs-copy-geps.py and mkdocs-generate-conformance.py,
    triggering another build due to the changed content.
    
    The solution is to use MkDocs Files API to programatically add the files
    into the site instead of copying them into the site directory.
    
    See mkdocs/mkdocs#2544 for more information.

--- hack/mkdocs-copy-geps.py
+++ hack/mkdocs-copy-geps.py
@@ -15,11 +15,37 @@
 import shutil
 import logging
 from mkdocs import plugins
+from mkdocs.structure.files import File
+from pathlib import Path
 
-log = logging.getLogger('mkdocs')
+log = logging.getLogger(f'mkdocs.plugins.{__name__}')
 
 
 @plugins.event_priority(100)
-def on_pre_build(config, **kwargs):
-    log.info("copying geps")
-    shutil.copytree("geps", "site-src/geps", dirs_exist_ok=True)
+def on_files(files, config, **kwargs):
+    log.info("adding geps")
+
+    # Remove previous site-src/geps path
+    site_src_geps = Path('site-src/geps')
+    if site_src_geps.exists() and site_src_geps.is_dir():
+      log.info("Found gep/ directory in site-src/. Deleting...")
+      shutil.rmtree(site_src_geps)
+
+    for root, _, gep_files, in Path('geps').walk():
+
+      # Iterate over the all the files in the GEP folder and add them to the site
+      for filename in gep_files:
+        file_path = str(root / filename)
+
+        if files.get_file_from_path(file_path) is None:
+          new_file = File(
+            path=file_path,
+            src_dir='./',
+            dest_dir=config['site_dir'],
+            use_directory_urls=config['use_directory_urls']
+          )
+          new_file.generated_by = f'{__name__}'
+
+          files.append(new_file)
+
+    return files
